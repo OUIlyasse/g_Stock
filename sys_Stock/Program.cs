835155f1b1b3b787e954a26f0f03fdd0eee50c0d
Ilyasse Ilyasse <livezizoo.maag@gmail.com>
2022-12-08 22:55:49 +0100

    Creer les projets de la solution

---
+++ sys_Stock/Program.cs
@@ -0,0 +1,23 @@
+﻿using sys_Stock.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sys_Stock
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain());
+        }
+    }
+}
